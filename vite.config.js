8e32079cacf340d2529bbbd07ad6b55cef76549d
Faizal Chan <moefc32@gmail.com>
2025-02-13 19:08:27 +0700

    minor update

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+	],
 	ssr: {
 		noExternal: ['lucide-svelte'],
 	},
 	server: {
 		host: process.env.VITE_IPBIND || 'localhost',
-		port: parseInt(process.env.VITE_PORT) || 4000,
+		port: parseInt(process.env.VITE_PORT),
 	},
 });
