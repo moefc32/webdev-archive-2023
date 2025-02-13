a4177a09a667ed3b17e7d7eaf5bc6691a2b62898
Faizal Chan <moefc32@gmail.com>
2025-02-13 22:15:14 +0700

    tab to space indentations

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-	],
-	ssr: {
-		noExternal: ['lucide-svelte'],
-	},
-	server: {
-		host: process.env.VITE_IPBIND || 'localhost',
-		port: parseInt(process.env.VITE_PORT),
-	},
+    plugins: [
+        sveltekit(),
+    ],
+    ssr: {
+        noExternal: ['lucide-svelte'],
+    },
+    server: {
+        host: process.env.VITE_IPBIND || 'localhost',
+        port: parseInt(process.env.VITE_PORT),
+    },
 });
