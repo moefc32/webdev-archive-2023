57360c54098bba976c8062dcd2527340b2981f10
Faizal Chan <moefc32@gmail.com>
2025-02-17 04:12:04 +0700

    add .prettierrc and fix indentations

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,13 @@
 <script>
-  import "../app.css";
+    import '../app.css';
 
-  import Header from "$lib/component/Header.svelte";
-  import Footer from "$lib/component/Footer.svelte";
+    import Header from '$lib/component/Header.svelte';
+    import Footer from '$lib/component/Footer.svelte';
 </script>
 
 <svelte:head>
-  <meta property="og:title" content={import.meta.env.VITE_APP_NAME} />
-  <meta property="og:image" content="/favicon.png" />
+    <meta property="og:title" content={import.meta.env.VITE_APP_NAME} />
+    <meta property="og:image" content="/favicon.png" />
 </svelte:head>
 
 <Header />
